7eab6ffe81ff13ed74676eb816ad642252537555
Amirmohammad Masoumi <39368982+masoomi1396@users.noreply.github.com>
2023-08-29 09:04:37 +0330

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,7 +20,6 @@ addButton.addEventListener('click',(e)=>{
         appendChildToDoList(list);
         showInput();
         showCounter();
-        console.log("heloo")
     }
     else{
         alert("Please enter something!");
@@ -29,7 +28,6 @@ addButton.addEventListener('click',(e)=>{
     }
 })
 const createTodoList = (text) => {
-    console.log("text : "+text);
     const list = document.createElement('div');
     list.innerHTML = '<div class="flex flex-nowrap justify-start space-x-2 items-center w-auto"><i class="text-slate-200 text-2xl border border-black rounded-full w-[40px] h-[40px] flex justify-center items-center hover:cursor-pointer" onclick="checkedList(this)">⎷</i><h3 class="text-lg md:text-xl text-black font-medium">'+text+'</h3></div><i class="text-2xl font-bold  bg-clip-text text-red-500 w-[20px] hover:cursor-pointer" onclick="removeElement(this)">X</i>';
     list.className = 'w-full flex flex-nowrap justify-between items-center';
@@ -40,7 +38,6 @@ const appendChildToDoList = (element)=>{
     todoListCounter++;
 }
 const removeElement = (element) => {
-    console.log("Remove()")
     element.parentElement.remove();
     todoListCounter--;
     showCounter();
